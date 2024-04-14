f06ad8d1608ed00a579ba0d743ef12074f304144
Gabriel <gabicordoba1998@gmail.com>
2024-04-14 17:05:18 -0300

    Creacion de Clase Categoria e Imagen

---
+++ CatalogoWinForm/Imagen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogoWinForm
+{
+    internal class Imagen
+    {
+        public int ID { get; set; }
+        public string ImagenUrl { get; set; }
+        public int IDArticulo { get; set; }
+    }
+}
