c280801abd78d798f0cbafd0339510c3ec3c2955
aruuleon <arieleonchang@gmail.com>
2024-04-22 13:01:29 -0300

    Creación de Ventanas: ArticuloAjustes, CategoriaAjustes, MarcaAjustes. Esto va a permitir que se pueda agregar un elemento perteneciente a cada una de estas secciones o ver la lista completa de los elementos de cada una de las mismas.

---
+++ CatalogoWinForm/ArticuloAjustes.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace CatalogoWinForm {
+    partial class ArticuloAjustes {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnListaArticulos = new System.Windows.Forms.Button();
+            this.btnAgregarArticulo = new System.Windows.Forms.Button();
+            this.lblTituloInicio = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnListaArticulos
+            // 
+            this.btnListaArticulos.Location = new System.Drawing.Point(302, 303);
+            this.btnListaArticulos.Name = "btnListaArticulos";
+            this.btnListaArticulos.Size = new System.Drawing.Size(197, 49);
+            this.btnListaArticulos.TabIndex = 24;
+            this.btnListaArticulos.Text = "LISTA ARTICULOS";
+            this.btnListaArticulos.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregarArticulo
+            // 
+            this.btnAgregarArticulo.Location = new System.Drawing.Point(302, 228);
+            this.btnAgregarArticulo.Name = "btnAgregarArticulo";
+            this.btnAgregarArticulo.Size = new System.Drawing.Size(197, 49);
+            this.btnAgregarArticulo.TabIndex = 23;
+            this.btnAgregarArticulo.Text = "AGREGAR ARTICULO";
+            this.btnAgregarArticulo.UseVisualStyleBackColor = true;
+            // 
+            // lblTituloInicio
+            // 
+            this.lblTituloInicio.AutoSize = true;
+            this.lblTituloInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloInicio.Location = new System.Drawing.Point(331, 99);
+            this.lblTituloInicio.Name = "lblTituloInicio";
+            this.lblTituloInicio.Size = new System.Drawing.Size(131, 25);
+            this.lblTituloInicio.TabIndex = 22;
+            this.lblTituloInicio.Text = "ARTICULOS";
+            // 
+            // ArticuloAjustes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnListaArticulos);
+            this.Controls.Add(this.btnAgregarArticulo);
+            this.Controls.Add(this.lblTituloInicio);
+            this.Name = "ArticuloAjustes";
+            this.Text = "ArticuloAjustes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListaArticulos;
+        private System.Windows.Forms.Button btnAgregarArticulo;
+        private System.Windows.Forms.Label lblTituloInicio;
+    }
+}
